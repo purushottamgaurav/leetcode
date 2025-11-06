71c0c441142361e185ffb8f1e5f5241771ef9437
Purushottam Gaurav <gaurav.purushottam@sapienceanalytics.com>
2025-11-06 11:33:52 +0530

    ques 1 easy

---
+++ LeetCode/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter the question no to execute: -");
+Console.WriteLine("1. Two Sum");
+int quesNo = int.Parse(Console.ReadLine());
+
+switch (quesNo)
+{
+    case 1:
+        int[] nums = [2, 11, 7, 15];
+        var response = TwoSum(nums, 9);
+        Console.WriteLine($"{response[0]}, {response[1]}");
+        break;
+    default:
+        Console.WriteLine("Invalid question number.");
+        break;
+}
+
+int[] TwoSum(int[] nums, int target)
+{
+    int length = nums.Length;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = i + 1; j < length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return [i, j];
+            }
+        }
+
+    }
+    return [];
+}
